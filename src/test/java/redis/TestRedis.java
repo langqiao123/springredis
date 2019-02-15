45e8ed13aaac233c0d703fb46839f7578d8fe39e
yangmingyang <yangmingyang@ppdai.com>
2019-02-15 17:44:35 +0800

    commit

--- src/test/java/redis/TestRedis.java
+++ src/test/java/redis/TestRedis.java
@@ -5,12 +5,14 @@ import redis.clients.jedis.Jedis;
 public class TestRedis {
 	public static void main(String[] args) {
 		 //连接本地的 Redis 服务
-	      Jedis jedis = new Jedis("10.200.11.153",6378);
+	      Jedis jedis = new Jedis("192.168.211.15",6379);
 	      //jedis.auth("123456");
 	      System.out.println("Connection to server sucessfully");
 	      //查看服务是否运行
 	      System.out.println("Server is running: "+jedis.ping());
-	      
+	      String value = jedis.get("RK_USER_LOGIN_TOKEN_PRE_20180418142053137990");
+	      value.getBytes();
+	      System.out.println(jedis.get("RK_USER_LOGIN_TOKEN_PRE_20180418142053137990"));
 	      /*jedis.set("foo", "bar");  
 	      String value = jedis.get("foo");  
 	      System.out.println(value);  */
